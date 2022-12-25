72bffc1880dfafc1f9169b62c7df1e85de1e6e1b
Ron <ronchick.weiss@gmail.com>
2022-12-25 19:55:34 +0200

    removed redundant code

--- Main.py
+++ Main.py
@@ -1,12 +1,7 @@
 from Windows.MainWindow import MainWindow
 # from yeelight import discover_bulbs # Fututre feature - auto-discover yealink bulbs using this
 
-# import Windows.SettingsWindow as SettingsWindow, Utils.ConfigurationManager as ConfigurationManager, Utils.HALightChanger as HALightChanger
-
 from Utils.ConfigurationManager import ConfigurationManager
-from Utils.ILightChanger import ILightChanger
-from Utils.HALightChanger import HALightChanger
-from Utils.YeeLightChanger import YeeLightChanger
 from Utils.LightChangerResolver import LightChangerResolver
 from Utils.ScreenReader import ScreenReader
 from Utils.RGBToHSVConverter import RGBToHSVConverter
